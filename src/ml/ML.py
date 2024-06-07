6b7234dbdfc1f208e44385590e27801f912021f2
dmitrii <dmitriizagorodnikov@gmail.com>
2024-06-07 18:05:19 -0500

    lol

--- src/ml/ML.py
+++ src/ml/ML.py
@@ -1,1 +1,10 @@
-print('жепа')
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+all_events_pickle_file_path = '/Users/dmitrii/Desktop/Хакатон/Datasets/All_events.pkl'
+all_events = pd.read_pickle(all_events_pickle_file_path)
+
+print(all_events)
+
