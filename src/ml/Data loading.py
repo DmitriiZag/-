87215896b560a334d0a2657f2424bcdd462684f0
dmitrii <dmitriizagorodnikov@gmail.com>
2024-06-09 11:17:15 -0500

    lol

--- src/ml/Data loading.py
+++ src/ml/Data loading.py
@@ -18,32 +18,52 @@ else:
     people.to_pickle(pickle_file_path)
 '''
 
-
-
-
-
 set_5 = pd.read_pickle('/Users/dmitrii/Desktop/Хакатон/Datasets/Set 5.pkl')
-print('done')
 set_6 = pd.read_pickle('/Users/dmitrii/Desktop/Хакатон/Datasets/Set 6.pkl')
-print('done')
 set_9 = pd.read_pickle('/Users/dmitrii/Desktop/Хакатон/Datasets/Set 9.pkl')
-print('done')
 set_11 = pd.read_pickle('/Users/dmitrii/Desktop/Хакатон/Datasets/Set 11.pkl')
-print('done')
 set_14 = pd.read_pickle('/Users/dmitrii/Desktop/Хакатон/Datasets/Set 14.pkl')
-print('done')
 
 # Step 1: Shuffle set_5 and select 10%
 set_5_shuffled = set_5.sample(frac=1, random_state=1)  # Shuffle
 subset_set_5 = set_5_shuffled.head(int(0.1 * len(set_5)))  # Take 10%
-print('done')
+
+# Convert columns to numeric, setting errors='coerce' will convert non-numeric values to NaN
+columns_to_convert = [
+    'Объём поданого теплоносителя в систему ЦО',
+    'Объём обратного теплоносителя из системы ЦО',
+    'Разница между подачей и обраткой(Подмес)',
+    'Разница между подачей и обраткой(Утечка)',
+    'Температура подачи',
+    'Температура обратки',
+    'Наработка часов счётчика',
+    'Расход тепловой энергии '
+]
+
+for column in columns_to_convert:
+    set_11[column] = pd.to_numeric(set_11[column], errors='coerce')
+
+# Now perform the aggregation
+set_11 = set_11.groupby('УНОМ').agg({
+    'Объём поданого теплоносителя в систему ЦО': 'mean',
+    'Объём обратного теплоносителя из системы ЦО': 'mean',
+    'Разница между подачей и обраткой(Подмес)': 'mean',
+    'Разница между подачей и обраткой(Утечка)': 'mean',
+    'Температура подачи': 'mean',
+    'Температура обратки': 'mean',
+    'Наработка часов счётчика': 'mean',
+    'Расход тепловой энергии ': 'mean'
+}).reset_index()
+
 
 result = subset_set_5.copy()  # Start with the 10% subset of set_5
 result = pd.merge(result, set_6, on='УНОМ', how='left', suffixes=('', '_set6'))
 result = pd.merge(result, set_9, on='УНОМ', how='left', suffixes=('', '_set9'))
-#result = pd.merge(result, set_11, on='УНОМ', how='left', suffixes=('', '_set11'))
+result = pd.merge(result, set_11, on='УНОМ', how='left', suffixes=('', '_set11'))
 result = pd.merge(result, set_14, on='УНОМ', how='left', suffixes=('', '_set14'))
-print('done')
+
+
+
 
 
 
